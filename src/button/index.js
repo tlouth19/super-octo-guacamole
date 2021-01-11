e67bce157cbf4c8c824978900803033394ac94a6
Tyson Louth <tysonlouth@gmail.com>
2021-01-11 11:38:57 -0500

    Initial commit

---
+++ src/button/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Button = (props) => {
+  const { onClick, children } = props;
+  return <button onClick={onClick}>{children}</button>;
+};
+
+export default Button;
